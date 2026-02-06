6c13959226be6fe2da54d512cbbf24e224e21b7c
sadunikasehan10-ux <sadunikasehan10@gmail.com>
2026-02-07 03:02:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,74 @@
-body{
-    font-family: Arial;
-    background: #f2f2f2;
-    text-align: center;
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+import {
+  getDatabase,
+  ref,
+  push,
+  get,
+  child
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD5xIjemUx_rH4TzFBW_TJQ0Q7crdJ7IvY",
+  authDomain: "wasity-trip.firebaseapp.com",
+  databaseURL: "https://wasity-trip-default-rtdb.firebaseio.com",
+  projectId: "wasity-trip"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+
+function getDeviceInfo(){
+   return navigator.userAgent;
 }
 
-.container{
-    background: white;
-    width: 300px;
-    margin: 100px auto;
-    padding: 20px;
-    border-radius: 10px;
-    box-shadow: 0px 0px 10px gray;
+async function getIP(){
+    let res = await fetch("https://api.ipify.org?format=json");
+    let data = await res.json();
+    return data.ip;
 }
 
-input, select{
-    width: 80%;
-    padding: 8px;
-    font-size: 16px;
-}
 
-button{
-    padding: 10px 20px;
-    background: green;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    font-size: 16px;
-}
+window.submitVote = async function(){
 
-button:hover{
-    background: darkgreen;
+    const name = document.getElementById("name").value;
+    const vote = document.getElementById("vote").value;
+
+    if(name === ""){
+        alert("නම දාන්න");
+        return;
+    }
+
+    const ip = await getIP();
+    const device = getDeviceInfo();
+    const time = new Date().toLocaleString();
+
+    const snapshot = await get(ref(db,"votes"));
+
+    let voted = false;
+
+    if(snapshot.exists()){
+        snapshot.forEach(child => {
+            if(child.val().ip === ip){
+                voted = true;
+            }
+        });
+    }
+
+    if(voted){
+        alert("ඔබ දැනටමත් vote කරලා!");
+        return;
+    }
+
+    await push(ref(db,"votes"),{
+        name: name,
+        vote: vote,
+        ip: ip,
+        device: device,
+        time: time
+    });
+
+    alert("Vote Saved!");
 }
